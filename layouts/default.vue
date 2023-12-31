82b69c73e93d25a075a376795ad01250c3f0896c
Jan Muixi <you@example.com>
2023-12-31 17:23:11 +0100

    adding meta tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,4 +8,12 @@
 <script setup lang="ts">
     const store = useCarsStore()
     store.getStartingData()
+
+    useHead({
+        title: 'CommonShare car catalog',
+        meta: [{
+            name: 'description',
+            content: 'This is a car catalog developed in Nuxt3'
+        }]
+    })
 </script>
